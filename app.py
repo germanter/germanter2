13e6154efd201f48991d2fab19095766573d92bd
germanter <usersuer32@gmail.com>
2024-12-24 01:11:25 +0400

    homepage added

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-from flask import Flask
+from flask import Flask,render_template
+                
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/market')
+def market():
+    return render_template('market.html')
+
+if __name__ == '__main__':
+    app.run(
+        host = '0.0.0.0',
+        debug = True
+    )
