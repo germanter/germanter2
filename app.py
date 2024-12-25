18b391dfbfae83078004bc5eb8691cd9ebc780da
germanter <usersuer32@gmail.com>
2024-12-25 22:31:54 +0400

    login signup base

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask,render_template,jsonify
 from database import load_data,load_item  
+import random
+import datetime
                 
 app = Flask(__name__)
+
+def set_reservation():
+    tomorrow=datetime.datetime.now() + datetime.timedelta(days=1)
+    tomorrow=tomorrow.strftime("%Y-%m-%d")
+    times=['10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00']
+    time=random.choice(times)
+    return [tomorrow,time]
 
 @app.route('/')
 def home():
@@ -16,6 +25,20 @@ def market():
 def show_item(id):
     data = load_item(id)
     return render_template('show_item.html',data=data)
+
+@app.route('/market/<id>/reservation')
+def reservation(id):
+    data = load_item(id)
+    timeset= set_reservation()
+    return render_template('reservation.html',data=data,timeset=timeset)
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(
